69057da6fa794e93f7687e3b182a0f138953e78b
Brad Johnson <johnson.brad15@gmail.com>
2025-06-16 20:45:32 -0500

    Instrument context and useReducer with stateupdater class

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import DataManager from './components/DataManager';
 import StockData from './components/data';
-import Test from './components/test';
-import Ladder from './components/InstrumentLevelView';
+import InstrumentOnDay from './components/InstrumentOnDay';
+import InstrumentLevelView from './components/InstrumentLevelView';
 // import FinChart from './components/chart';
 import Sparkline from './components/sparkline'
 import DockManager from './components/DockManager_';
 import FinChart from './components/testchart';
+
+import { InstrumentProvider, useInstrument } from './components/InstrumentContext';
 
 function App() {
   // console.log("App");
@@ -17,14 +19,18 @@ function App() {
   };
 
   return (
-    <div style={{height:500}}>
-      <DataManager url="ws://localhost:8765" onDataChange={handleDataChange} />
-      {/* <StockData onDataChange={handleDataChange} /> */}
-      {/* <DockManager sharedData={sharedData} /> */}
-      {/* <div style={{height:500}}><FinChart sharedData={sharedData} /></div> */}
-      {/* <div style={{width:150, height:20}}><Sparkline sharedData={sharedData} /></div> */}
-      <div style={{width:200, height:500}}><Ladder sharedData={sharedData}/></div>
-    </div>
+    <InstrumentProvider>
+      <div style={{width:200, height:500}}>
+        <DataManager url="ws://localhost:8765" onDataChange={handleDataChange} />
+        <InstrumentOnDay style={{width:'100%', height:'30px'}} data={sharedData}/>
+        {/* <StockData onDataChange={handleDataChange} /> */}
+        {/* <DockManager sharedData={sharedData} /> */}
+        {/* <div style={{height:500}}><FinChart sharedData={sharedData} /></div> */}
+        <div style={{width:'100%', height:35}}><Sparkline sharedData={sharedData} /></div>
+        {/* <div className="InstrumentLevelViewContainer" style={{width:200, height:500}}><InstrumentLevelView sharedData={sharedData}/></div> */}
+        <InstrumentLevelView initialWidth={200} sharedData={sharedData}/>
+      </div>
+    </InstrumentProvider>
   );
 }
 
--- src/components/sparkline.js
+++ src/components/sparkline.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { Sparklines, SparklinesLine, SparklinesBars, SparklinesSpots } from 'react-sparklines';
+import { Sparklines, SparklinesLine, SparklinesArea, SparklinesBars, SparklinesSpots } from 'react-sparklines';
+import '../App.css';
+import { useInstrument } from './InstrumentContext';
 
 function dataExists(props) {
     return (props != null 
@@ -12,38 +14,52 @@ function dataExists(props) {
 let lastDataSize = 0;
 
 function Sparkline(props) {
+    const { data: instrument } = useInstrument();
     const [data, setData] = useState([]);
     const [sparkData, setSparkData] = useState([]);
+    const [lastData, setLastData] = useState(null);
 
     useEffect(() => {
         if (dataExists(props)) {
-            // console.log("Updating data");
             setData(props.sharedData);
         }
     },);
 
     useEffect(() => {
         if  (dataExists(props) && data.sparkData.length > lastDataSize) {
-            // console.log("Updating sparkData");
             lastDataSize = data.sparkData.length;
             setSparkData([...sparkData, data.sparkData[data.sparkData.length-1]]); 
-            // console.log(sparkData);
+            setLastData(data.sparkData[data.sparkData.length-1]);
         }
     },[data]);
 
-    if (! dataExists(props))
-    {
-        console.log("Null/undefined data");
-        return (<div>No Data</div>);
-    }
-    // setSparkData([...sparkData, props.sharedData.sparkData[props.sharedData.sparkData.length-1]]); 
+    const style = {
+        backgroundColor: "rgb(95, 95, 95)",
+        width: "100%",
+        height:"100%",
+        borderTop: 0,
+        borderLeft: "1px solid black",
+        borderRight: "1px solid black",
+    };
+
+    // if (! dataExists(props) || !instrument)
+    // {
+    //     console.log("Sparkline: Null/undefined data");
+    //     return (<div className='dkGreyBg' style={style}></div>);
+    // }
+
+    const openingPx = instrument ? instrument.openingPx : lastData;
 
     return (
-        <Sparklines data={sparkData} limit={50} width={100} height={20} margin={5}>
-            <SparklinesBars style={{ fill: "#41c3f9", fillOpacity: ".25" }} />
-            <SparklinesLine style={{ stroke: "#41c3f9", fill: "none" }} />
-            <SparklinesSpots />
-        </Sparklines>
+        <div className='dkGreyBg' style={style}>
+            <Sparklines data={sparkData} limit={50} width={100} height={20} margin={5}>
+                <SparklinesLine 
+                    style={{ strokeWidth: 0.5, stroke: (lastData == openingPx ? "#FFFFFF" : lastData > openingPx ? "#90ff81" : "#ff6767"), fill: "none"}} 
+                    // style={{ strokeWidth: 0.5, fill: (lastData == instrumentData.openingPx ? "#FFFFFF" : lastData > instrumentData.openingPx ? "#90ff81" : "#ff6767"), stroke: "white"}} 
+                />  
+                <SparklinesSpots size={0}/>
+            </Sparklines>
+        </div>
     );
 }
 
